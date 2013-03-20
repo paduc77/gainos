52064a31bc63653cc987f516fe11709c0689a9db
parai <parai@foxmail.com>
2013-03-20 13:39:47 +0000

    "New"

---
+++ GaInOS_Based_On_uTenux-tkernel/gainos/uTenux/include/sys/sysdepend/app_mpc56xx/machine_depend.h
@@ -0,0 +1,85 @@
+/*
+ *------------------------------------------------------------------------------
+ *    Include/sys
+ *
+ *    Copyright (C) 2008-2013 by Fan Wang(parai). All rights reserved.
+ *
+ *    This program is open source software; developer can redistribute it and/or
+ *    modify it under the terms of the U-License as published by the T-Engine China
+ *    Open Source Society; either version 1 of the License, or (at developer option)
+ *    any later Version.
+ *
+ *    This program is distributed in the hope that it will be useful,but WITHOUT ANY
+ *    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ *    A PARTICULAR PURPOSE.  See the U-License for more details.
+ *
+ *    Developer should have received a copy of the U-License along with this program;
+ *    if not, download from www.tecoss.org(the web page of the T-Engine China Open
+ *    Source Society).
+ *
+ *    CPU:        MC9S12DP512
+ *    RTOS:       uT-Kernel
+ *    Version:    1.4.00
+ *
+ *	 File Name      : machine_depend.h
+ *	 Create Date    : 2013/3/15-2013/3/17
+ *	 Author	        : Fan Wang(parai)
+ *-------------------------------------------------------------------------------
+ */
+
+#ifndef __SYS_MACHINE_DEPEND_H__
+#define __SYS_MACHINE_DEPEND_H__
+
+
+#undef _APP_MPC56XX_
+
+#define _APP_MPC56XX_		    1
+#define CPU_E200Z		        1
+#define CPU_MPC56XX	        	1
+#define APP_MPC56XX		        1
+
+
+#define ALLOW_MISALIGN		        0
+#define BIGENDIAN		            0
+#define VIRTUAL_ADDRESS		        0
+#define ALLOCA_NOSPT		        0
+#define INT_BITWIDTH		        32
+
+#ifndef _Csym
+#define _Csym			            0
+#endif
+
+#define configRTI  0
+#define configDEC  1
+#define configTickSrc configDEC
+
+#define CPU_FREQUENCY  64   /* 64 MHZ */
+#define OSC_FREQUENCY  8    /* Oscillator Clock 8MHZ */
+#if(CPU_FREQUENCY==8)
+#define SYNCR_V 0x36100000
+#endif
+#if(CPU_FREQUENCY==16)
+#define SYNCR_V 0x16100000
+#endif
+#if(CPU_FREQUENCY==20)
+#define SYNCR_V 0x18100000
+#endif
+#if(CPU_FREQUENCY==32)
+#define SYNCR_V 0x16080000
+#endif
+#if(CPU_FREQUENCY==40)
+#define SYNCR_V 0x18080000
+#endif
+#if(CPU_FREQUENCY==50)
+#define SYNCR_V 0x1A880000
+#endif
+#if(CPU_FREQUENCY==60)
+#define SYNCR_V 0x15800000
+#endif
+#if(CPU_FREQUENCY==64)
+#define SYNCR_V 0x16000000	/**PREFDIV=1,MFD=12,RFD=0,->Fsys=64MHZ*/
+#endif
+#if(CPU_FREQUENCY==80) /**PREFDIV=1,MFD=16,RFD=0,->Fsys=80MHZ*/
+#define SYNCR_V 0x18000000
+#endif
+#endif /* __SYS_MACHINE_DEPEND_H__ */
---
+++ GaInOS_Based_On_uTenux-tkernel/gainos/uTenux/kernel/cpu/src/sysdepend/s12cpuv2/cpu_support.c
@@ -0,0 +1,133 @@
+/*
+ *------------------------------------------------------------------------------
+ *    Cpu
+ *
+ *    Copyright (C) 2008-2013 by Dalian uLoong Co.,Ltd. All rights reserved.
+ *
+ *    This program is open source software; developer can redistribute it and/or
+ *    modify it under the terms of the U-License as published by the T-Engine China
+ *    Open Source Society; either version 1 of the License, or (at developer option)
+ *    any later Version.
+ *
+ *    This program is distributed in the hope that it will be useful,but WITHOUT ANY
+ *    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ *    A PARTICULAR PURPOSE.  See the U-License for more details.
+ *
+ *    Developer should have received a copy of the U-License along with this program;
+ *    if not, download from www.tecoss.org(the web page of the T-Engine China Open
+ *    Source Society).
+ *
+ *    CPU:        MC9S12
+ *    RTOS:       uT-Kernel
+ *    Version:    1.4.00
+ *
+ *	 File Name      : cpu_support.c
+ *   Create Date    : 2013/3/15-2013/3/17
+ *   Author         : Fan Wang(parai)
+ *	 Description    : CPU-Dependent dispatcher Operation.
+ *-------------------------------------------------------------------------------
+ */
+
+#include <machine.h>
+#include <tk/errno.h>
+#include <tk/sysdef.h>
+#include <tk/asm.h>
+#include <sys/sysinfo.h>
+
+#include "config.h"
+#include "utk_config.h"
+#if USE_TRAP | (USE_DBGSPT & USE_HOOK_TRACE)
+#include "isysconf.h"
+#endif
+#include "typedef.h"
+#include "tkdev_conf.h"
+#include "offset.h"
+#include "mc9s12dp512.h"
+IMPORT INT	knl_dispatch_disabled;
+IMPORT void	*knl_ctxtsk;
+IMPORT void	*knl_schedtsk;
+IMPORT void knl_low_pow( void );
+IMPORT UINT  knl_taskmode;
+IMPORT	INT  knl_taskindp;
+IMPORT 	 UB	 knl_tmp_stack[TMP_STACK_SZ];
+IMPORT UB l_sp_offset;
+/*
+ *    Function Name : knl_dispatch_to_schedtsk,knl_dispatch_entry,_ret_int_dispatch
+ *    Create Date   : 2009/12/27-2012/11/22
+ *    Author        : wangshb
+ *    Description   : Dispatcher,save contexts 'ssp' to TCB.include three parts.
+ *                    1.dispatch_to_schedtsk:
+ *                         Throw away the current contexts and forcibly dispatch to
+ *                         'schedtsk.'
+ *                         Called directly by jump (bx) but do not return.
+ *                         Called on the undefined stack state (undefined 'ssp').
+ *                         Called on the interrupt disable state.
+ *                    2.dispatch_entry:
+ *                         Normal dispatch processing.
+ *                         Called by PendSV exception.
+ *                    3._ret_int_dispatch:
+ *                         Called when dispatch is required by 'tk_ret_int().'
+ *    Param	        : none
+ *    Return Code   : none
+ */
+#pragma CODE_SEG __NEAR_SEG NON_BANKED
+static void l_dispatch0(void)
+{
+    #if USE_DBGSPT & USE_HOOK_TRACE
+    	hook_stop_jmp();
+    #endif
+    l_dispatch1:
+    asm sei;   //disable interrupt
+    if(knl_schedtsk==(void *)0)
+    {
+        knl_low_pow();
+        asm cli;  //enable interrupt
+        goto l_dispatch1;
+    }
+    l_dispatch2:
+    knl_ctxtsk=knl_schedtsk;
+    #if USE_DBGSPT & USE_HOOK_TRACE
+	hook_exec_jmp();		    /* Hook processing */
+	#endif
+	knl_dispatch_disabled=0;    /* Dispatch enable */
+	/* Context restore */
+	asm   ldx  knl_ctxtsk;
+	asm   ldab l_sp_offset;
+	asm   lds  b,x;       /* Restore 'ssp' from TCB */
+	asm   pula
+    asm   staa	$30	      /* restore PPAGE */
+    asm   puld;
+    asm   std   knl_taskmode  /* restore knl_taskmode */
+    asm   rti;   
+}
+void knl_dispatch_to_schedtsk(void)
+{
+    asm  lds #knl_tmp_stack:TMP_STACK_SZ   /* Set temporal stack */
+    knl_dispatch_disabled=1;    /* Dispatch disable */ 
+    knl_ctxtsk=(void *)0;
+    asm sei; 
+    asm jmp l_dispatch0;
+}
+
+interrupt 4 void knl_dispatch_entry(void)
+{
+_ret_int_dispatch:
+    knl_dispatch_disabled=1;    /* Dispatch disable */ 
+    asm   ldd   knl_taskmode  
+    asm   pshd;                 /* save knl_taskmode */
+    asm   ldaa	$30		        
+	asm   psha                  /* save ppage */
+	asm   ldx  knl_ctxtsk;
+	asm   ldab l_sp_offset;
+	asm   sts  b,x;            /* save 'ssp' to TCB */
+	knl_ctxtsk=(void*)0;
+	asm jmp l_dispatch0;  	    	
+}
+IMPORT void knl_timer_handler( void );
+interrupt 7 void OSTickISR(void)
+{  
+    CRGFLG &=0xEF;			// clear the interrupt flag  
+	asm cli;     /* enable interrupt */
+    asm call knl_timer_handler; 		
+}
+#pragma CODE_SEG DEFAULT
