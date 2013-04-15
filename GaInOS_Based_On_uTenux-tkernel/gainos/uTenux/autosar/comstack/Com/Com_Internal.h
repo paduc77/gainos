5657f6224d4ca4f9309c94f09d43577c97d9b5d7
parai <parai@foxmail.com>
2013-04-15 13:33:40 +0000

    一些Dev问题

--- GaInOS_Based_On_uTenux-tkernel/gainos/uTenux/autosar/comstack/Com/Com_Internal.h
+++ GaInOS_Based_On_uTenux-tkernel/gainos/uTenux/autosar/comstack/Com/Com_Internal.h
@@ -40,34 +40,44 @@ extern Com_Arc_Config_type Com_Arc_Config;
 
 #define DET_REPORTERROR(_x,_y,_z,_q) Det_ReportError(_x,_y,_z,_q)
 
-
 // Define macro for parameter check.
-//#define PDU_ID_CHECK(PduId,ApiId,...) \
 #define PDU_ID_CHECK(PduId,ApiId,_rv) \
 	if (PduId >= COM_N_IPDUS) { \
 		DET_REPORTERROR(COM_MODULE_ID, COM_INSTANCE_ID, ApiId, COM_INVALID_PDU_ID); \
 		return _rv; \
-	} \
+	} 
+	
+#define PDU_ID_CHECK_NO_RV(PduId,ApiId) \
+	if (PduId >= COM_N_IPDUS) { \
+		DET_REPORTERROR(COM_MODULE_ID, COM_INSTANCE_ID, ApiId, COM_INVALID_PDU_ID); \
+		return; \
+	}	
 
 #define VALIDATE_SIGNAL(SignalId, ApiId,_rv) \
 	if (SignalId >= COM_N_SIGNALS) { \
 		DET_REPORTERROR(COM_MODULE_ID, COM_INSTANCE_ID, ApiId, COM_INVALID_SIGNAL_ID); \
 		return _rv; \
-	} \
-
-
+	}
 #else
 
 #define DET_REPORTERROR(_x,_y,_z,_q)
 
 #define PDU_ID_CHECK(PduId,ApiId,_rv)
+
+#define PDU_ID_CHECK_NO_RV(PduId,ApiId)
+
 #define VALIDATE_SIGNAL(PduId, ApiId,_rv)
 #endif
 
-
+#if 0
 #define TESTBIT(source,bit)	( *( (uint8 *)source  + (bit / 8) ) &  (uint8)(1u << (bit % 8)) )
 #define SETBIT(dest,bit)	( *( (uint8 *)dest    + (bit / 8) ) |= (uint8)(1u << (bit % 8)) )
 #define CLEARBIT(dest,bit)	( *( (uint8 *)dest    + (bit / 8) ) &= (uint8)~(uint8)(1u << (bit % 8)) )
+#else  /* I hate "/" and "%"  operation which can be replaced by ">>" and  "&", @parai*/
+#define TESTBIT(source,bit)	( *( (uint8 *)source  + (bit >> 3U) ) &  (uint8)(1u << (bit & 7U)) )
+#define SETBIT(dest,bit)	( *( (uint8 *)dest    + (bit >> 3U) ) |= (uint8)(1u << (bit & 7U)) )
+#define CLEARBIT(dest,bit)	( *( (uint8 *)dest    + (bit >> 3U) ) &= (uint8)~(uint8)(1u << (bit & 7U)) )
+#endif
 
 
 #define GET_Signal(SignalId) \
